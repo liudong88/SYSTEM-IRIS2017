8173eae8a18566e0ccbdd1294ff5bc1e0309a9bb
tiger19816 <tiger19816@gmail.com>
2017-08-24 01:53:09 +0900

    RankingServletの作成

--- WebContent/ranking.jsp
+++ WebContent/ranking.jsp
@@ -1,11 +1,11 @@
-<%@ page 
-	language="java" 
+<%@ page
+	language="java"
 	contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"
 	import="java.util.ArrayList"
 	import="java.util.HashMap"
 %><%
-ArrayList<String[]> ranking = new ArrayList<String[]>(); // ranking 
+/*ArrayList<String[]> ranking = new ArrayList<String[]>(); // ranking
 ArrayList<String[]> spots   = new ArrayList<String[]>(); // spots   ...plz request.getAttribute
 String nowSpotID = ""; // nowSpotID 全スポットの場合は空
 //--dummy↓↓↓--
@@ -20,8 +20,12 @@ String dummySpots1[] = {"1", "舞鶴"};
 String dummySpots2[] = {"2", "呉"  };
 spots.add(dummySpots0);
 spots.add(dummySpots1);
-spots.add(dummySpots2);
+spots.add(dummySpots2);*/
 //--dummy↑↑↑--
+
+ArrayList<String[]> ranking = (ArrayList<String[]>)request.getAttribute("RANKING"); // ranking
+ArrayList<String[]> spots   = (ArrayList<String[]>)request.getAttribute("SPOTS"); // spots   ...plz request.getAttribute
+String nowSpotID = (String)request.getAttribute("NOWSPOT"); // nowSpotID 全スポットの場合は空
 
 String spotList    = "";
 String rankingList = "";
@@ -38,7 +42,7 @@ for(String[] data: ranking) {
 		tmpGood += "<img class=\"goodNum\" src=\"./images/ranking/num_"+num+".png\" style=\"left:-"+tmpCnt+"px;right:0;\">";
 		tmpCnt += 8;
 	}
-	
+
 	rankingList += "<tr><th rowspan=\"3\" style=\"text-wrap:none;text-align:left;border-style:solid;border-color:#ff6c6c;border-width:0 0 5px;\">"+tmpGood+"</th><td style=\"text-align:right;\">"+data[3]+"</td></tr>";
 	rankingList += "<tr><td><img src=\""+data[0]+"\"></td></tr>";
 	rankingList += "<tr><td style=\"border-style:solid;border-color:#ff6c6c;border-width:0 0 5px;\">"+data[1]+"</td></tr>";
@@ -88,7 +92,7 @@ if(nowSpotID.isEmpty()) {
 			<ul id="selectSpot">
 				<li onclick="selectOpen();"><span style="font-size:125%;">▼ スポット別</span> - <%=nowSpot %>
 					<ul>
-						<li><a href="">全スポット</a></li>
+						<li><a href="?">全スポット</a></li>
 						<%=spotList %>
 					</ul>
 				</li>
---
+++ src/RankingServlet.java
@@ -0,0 +1,133 @@
+
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hacku2017.DatabaseAccess;
+
+/**
+ * Servlet implementation class RankingServlet
+ */
+@WebServlet("/RankingServlet")
+public class RankingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RankingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//文字化け対策
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		//インスタンスの生成
+		DatabaseAccess dba = new DatabaseAccess();
+		//LoginCheck lc = new LoginCheck();
+
+		String strSpotId = request.getParameter("spot_id");
+
+		boolean whereFlag = true;
+		if(strSpotId == null){
+			whereFlag = false;
+			strSpotId = "";
+		}
+
+		String sql = "";
+		//写真の情報を取得するSQL
+		sql = "SELECT file_path,photo_explanation,user_name,"
+				+ "CASE WHEN good.good_cnt IS NULL THEN 0 ELSE good.good_cnt END AS g_cnt,p.photo_id "
+				+ "FROM ((photos AS p LEFT JOIN (SELECT photo_id,COUNT(*) AS good_cnt FROM photo_good GROUP BY photo_id) AS good ON p.photo_id = good.photo_id) "
+				+ "LEFT JOIN spot_photos ON p.photo_id = spot_photos.photo_id) "
+				+ "LEFT JOIN users ON p.user_id = users.user_id ";
+		if(whereFlag){
+			sql += "WHERE spot_id = " + strSpotId+ " ";
+		}
+		sql += "ORDER BY g_cnt DESC "
+				+ "LIMIT 25;";
+		System.out.println(sql);
+
+		ResultSet rs = null;
+
+		ArrayList<String[]> tbl = new ArrayList<String[]>();
+		try{
+			dba.Connect();
+			rs = dba.Select(sql);
+			while(rs.next()){
+				String[] rec = {"./upload/small/" + rs.getString("file_path"),rs.getString("photo_explanation"),rs.getString("g_cnt"),rs.getString("user_name"),rs.getString("p.photo_id")};
+				tbl.add(rec);
+			}
+		}catch (SQLException e) {
+			// TODO: handle exception
+		}finally{
+			if(rs != null){
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO: handle exception
+				}
+			}
+			dba.Close();
+		}
+
+		rs = null;
+
+		sql = "SELECT spot_id,spot_name FROM stamp_spots";
+		ArrayList<String[]> spotTbl = new ArrayList<String[]>();
+		try{
+			dba.Connect();
+			rs = dba.Select(sql);
+			while(rs.next()){
+				String[] rec = {rs.getString("spot_id"),rs.getString("spot_name")};
+				spotTbl.add(rec);
+			}
+		}catch (SQLException e) {
+			// TODO: handle exception
+		}finally{
+			if(rs != null){
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO: handle exception
+				}
+			}
+			dba.Close();
+		}
+
+		request.setAttribute("RANKING", tbl);
+		request.setAttribute("SPOTS", spotTbl);
+		request.setAttribute("NOWSPOT", strSpotId);
+		RequestDispatcher rd = request.getRequestDispatcher("ranking.jsp");
+		rd.forward(request, response);
+
+		//【画像パス、画像説明、良いね数、ユーザ名、画像ID】
+		//【スポットID、スポット名】
+		//NowSpotId
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
